d279df7a1853ef2c0a4fc99b73f8719ab4547e68
Jeremy <JeremyPlott@gmail.com>
2019-08-29 21:36:00 -0400

    initial commit

--- PWListHomework/Program.cs
+++ PWListHomework/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PWListHomework {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var PWDict = new Dictionary<string, string>();
+            PWDict.Add("YouTube", "password1");
+            PWDict.Add("Yahoo", "password2");
+            PWDict.Add("gmail", "password3");
+            PWDict.Add("GitHub", "password4");
+            PWDict.Add("Bank", "password5");
+            PWDict.Add("Wikipedia", "password6");
+            PWDict.Add("RuneScape", "password7");
+            PWDict.Add("BDO", "password8");
+            PWDict.Add("Maplestory", "password9");
+            PWDict.Add("PassList", "password10");
+            PWDict.Add("Car", "password11");
+            PWDict.Add("Glasses", "password12");
+
+            foreach(var password in PWDict) {
+                Console.WriteLine(password);
+            }
+
         }
     }
 }
